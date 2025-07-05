563b972b6a8eee111edcd8e999790128260d415b
InGentleWave <freya6366@gmail.com>
2025-07-05 14:53:05 +0900

    Add Project0705

---
+++ SpringCRUD0705/src/main/java/kr/or/ddit/SpringCrud0705Application.java
@@ -0,0 +1,13 @@
+package kr.or.ddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrud0705Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrud0705Application.class, args);
+	}
+
+}
---
+++ SpringCRUD0705/src/test/java/kr/or/ddit/SpringCrud0705ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrud0705ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
